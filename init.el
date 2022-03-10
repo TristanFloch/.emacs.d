76d0af70ef8fce1bb5b1fe0775efdf8402da8f6e
Tristan FLOCH <tristan.floch@epita.fr>
2022-03-10 22:53:43 +0100

    misc

--- init.el
+++ init.el
@@ -32,3 +32,16 @@
 ;;  ;; Your init file should contain only one such instance.
 ;;  ;; If there is more than one, they won't work right.
 ;;  )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(vertico evil-magit forge magit which-key use-package toc-org sudo-edit smex rainbow-delimiters org-bullets ivy-rich hydra helpful general evil-nerd-commenter evil-collection emojify doom-themes doom-modeline dashboard counsel-projectile auto-package-update all-the-icons-dired)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
