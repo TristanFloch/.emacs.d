1ac3cf223155a95549cf501df948082f3c356326
Tristan FLOCH <tristan.floch@epita.fr>
2022-03-11 16:08:36 +0100

    init autogenerated stuff

--- init.el
+++ init.el
@@ -9,9 +9,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(consult forge magit helpful evil-nerd-commenter projectile rainbow-delimiters toc-org org-bullets all-the-icons-dired hydra general which-key vertico use-package page-break-lines orderless marginalia evil-collection emojify doom-themes dashboard auto-package-update all-the-icons)))
-(custom-set-faces
+
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
